d6904094949e344ac4b84a96742f5cf08e34588e
Senthilnathan2001 <senthilbca2001@gmail.com>
2025-04-29 17:40:55 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
             <div className='bg-gray-400 px-5 py-20'>
                 <div className='flex justify-center items-center'>
                     <p>Footer</p>
-                    <p>Hello</p>
                 </div>
             </div>
         </>
